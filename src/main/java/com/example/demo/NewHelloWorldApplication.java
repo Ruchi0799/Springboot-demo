699f95e177f699d4f4c5e5585cfe038a06ba54e4
Ruchi Mandve <ruchi.mandve@morningstar.com>
2021-09-23 16:36:32 +0530

    [ruchi]: implemented add logger to log project

--- src/main/java/com/example/demo/NewHelloWorldApplication.java
+++ src/main/java/com/example/demo/NewHelloWorldApplication.java
@@ -1,5 +1,9 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+//import org.apache.logging.log4j.Logger;
+//import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -10,12 +14,17 @@ import com.example.demo.controller.HelloRestController;
 @SpringBootApplication
 public class NewHelloWorldApplication {
 
+	public static final Logger logger=LoggerFactory.getLogger(NewHelloWorldApplication.class);
+	
 	public static void main(String[] args) {
-		System.out.println("Hello from bridgelabz from Ruchi ");
+		//System.out.println("Hello from bridgelabz from Ruchi ");
+		logger.debug("Welcome to Spring concept demo");
 		ApplicationContext context=SpringApplication.run(NewHelloWorldApplication.class, args);
 		DemoBean demoBean=context.getBean(DemoBean.class);
-		System.out.println("Demo Bean= "+demoBean.toString());
-		System.out.println(context.getBean(HelloRestController.class));
+		//System.out.println("Demo Bean= "+demoBean.toString());
+		//System.out.println(context.getBean(HelloRestController.class));
+		logger.debug("Demo Bean= "+demoBean.toString());
+		//logger.warn(context.getBean(HelloRestController.class));
 		//ApplicationContext context=SpringAppplication.run(NewHell)
 	}
 
--- src/main/java/com/example/demo/component/EmployeeBean.java
+++ src/main/java/com/example/demo/component/EmployeeBean.java
@@ -3,8 +3,6 @@ package com.example.demo.component;
 import org.springframework.stereotype.Component;
 
 @Component
-public class DemoBean{
-	public DemoBean() {
-		
-	}
+public class EmployeeBean{
+	
 }
